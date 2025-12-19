7ee336dbf8cc57e9f72ae5fabab6df4241e3998f
snoopies <cngz.gva@gmail.com>
2025-12-19 16:11:50 +0800

    Create install-proxy.sh

---
+++ install-proxy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install squid
+
+if [ ! -e "./squid.conf" ]; then
+   cat> ./squid.conf<<EOF
+      http_access allow all
+   EOF
+fi
+
+sudo cp ./squid.conf /etc/squid/
+sudo systemctl restart squid
+
