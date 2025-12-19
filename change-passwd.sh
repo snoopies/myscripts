e9ecd176195c88a26b5c08a0f6b829b6509c12f9
snoopies <cngz.gva@gmail.com>
2025-12-19 16:01:38 +0800

    Add password change script for user management
    This script checks if the USER_PASS variable is set and updates the user's password using the provided value.

---
+++ change-passwd.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [[ -z "$USER_PASS" ]]; then
+  echo "Please set 'USER_PASS' for user: $USER"
+  exit 3
+fi
+echo "### Update user: $USER password ###"
+echo -e "$USER_PASS\n$USER_PASS" | sudo passwd "$USER"
