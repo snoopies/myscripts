1c91899e09f2d59c620c48ef1d40cc92e1c3bc75
snoopies <cngz.gva@gmail.com>
2025-12-19 16:32:33 +0800

    Update install-proxy.sh

--- install-proxy.sh
+++ install-proxy.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-
-sudo apt update
-sudo apt install squid
-
-if [ ! -e "./squid.conf" ]; then
-   cat> ./squid.conf<<EOF
-      http_access allow all
-EOF
+if [ ! command -v apt >/dev/null 2>&1 ]; then
+   echo "没有apt，请安装！"
+   exit 3
 fi
 
+sudo apt update
+sudo apt install -y squid
+
+command -v wget > /dev/null 2>&1 || sudo apt install -y wget
+
+if [ ! -e "./squid.conf" ]; then
+#   cat> ./squid.conf<<EOF
+#      http_access allow all
+#EOF
+    wget https://raw.githubusercontent.com/snoopies/myscripts/refs/heads/main/squid.conf
 sudo cp ./squid.conf /etc/squid/
 sudo systemctl restart squid
 
