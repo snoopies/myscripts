83d8849ac0ffd836d52222ffd13ed47494b0f54a
snoopies <cngz.gva@gmail.com>
2025-12-19 16:39:53 +0800

    Update install-easytier.sh

--- install-easytier.sh
+++ install-easytier.sh
@@ -4,19 +4,21 @@ if [[ -z "$network_secret" ]]; then
 exit 2
 fi
 
+command -v wget > /dev/null 2>&1 || sudo apt install -y wget
+if [ ! -e "./easytier-linux-x86_64-v2.2.4.zip" ]; then
+   wget https://raw.githubusercontent.com/snoopies/myscripts/refs/heads/main/easytier-linux-x86_64-v2.2.4.zip
+fi
+
 #command -v unzip > /dev/null 2>&1 || sudo apt install -y unzip
 if [ ! command -v unzip >/dev/null 2>&1 ]; then
    sudo apt install -y unzip
-fi
-
-if [ ! -e "./easytier-linux-x86_64-v2.2.4.zip ]; then
-   wget 
 fi
 
 unzip -qo easytier-linux-x86_64-v2.2.4.zip
 cd easytier-linux-x86_64
 
 #nohup sudo ./easytier-core  --network-name default  --network-secret "$network_secret" -p wss://claw.now.cc:443 -i 10.1.1.100/24 --no-tun &
+
 sudo mkdir -p /etc/et
 
 MY_DIR=$(pwd)
