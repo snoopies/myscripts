33b32d9796f2fd52f762be89b8c92d6e49c51d09
snoopies <cngz.gva@gmail.com>
2025-12-20 21:55:24 +0800

    Update install-cloudflared.sh

--- install-cloudflared.sh
+++ install-cloudflared.sh
@@ -1,12 +1,15 @@
-# Add cloudflare gpg key
-sudo mkdir -p --mode=0755 /usr/share/keyrings
-curl -fsSL https://pkg.cloudflare.com/cloudflare-public-v2.gpg | sudo tee /usr/share/keyrings/cloudflare-public-v2.gpg >/dev/null
+#!/bin/bash
 
-# Add this repo to your apt repositories
-echo 'deb [signed-by=/usr/share/keyrings/cloudflare-public-v2.gpg] https://pkg.cloudflare.com/cloudflared any main' | sudo tee /etc/apt/sources.list.d/cloudflared.list
 
-# install cloudflared
-sudo apt-get update && sudo apt-get install cloudflared
+## Add cloudflare gpg key
+#sudo mkdir -p --mode=0755 /usr/share/keyrings
+#curl -fsSL https://pkg.cloudflare.com/cloudflare-public-v2.gpg | sudo tee /usr/share/keyrings/cloudflare-public-v2.gpg >/dev/null
+
+## Add this repo to your apt repositories
+#echo 'deb [signed-by=/usr/share/keyrings/cloudflare-public-v2.gpg] https://pkg.cloudflare.com/cloudflared any main' | sudo tee /etc/apt/sources.list.d/cloudflared.list
+
+## install cloudflared
+#sudo apt-get update && sudo apt-get install cloudflared
 
 #run the tunnel manually in your current terminal session only:
 cloudflared tunnel run --token "$my_cf_token"
