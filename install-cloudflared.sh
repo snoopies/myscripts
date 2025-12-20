b61e8e1d6eac5be8106c7db5ccb3594214574b2d
snoopies <cngz.gva@gmail.com>
2025-12-20 22:05:08 +0800

    Update install-cloudflared.sh

--- install-cloudflared.sh
+++ install-cloudflared.sh
@@ -15,12 +15,20 @@ fi
 ## install cloudflared
 #sudo apt-get update && sudo apt-get install cloudflared
 
+command -v wget > /dev/null 2>&1 || sudo apt install -y wget
+command -v unzip > /dev/null 2>&1 || sudo apt install -y unzip
+if [ ! -e "./cloudflared-linux-amd64.zip" ]; then
+   wget https://raw.githubusercontent.com/snoopies/myscripts/refs/heads/main/cloudflared-linux-amd64.zip
+fi
 
+unzip -qo cloudflared-linux-amd64.zip
 
-#run the tunnel manually in your current terminal session only:
-cloudflared tunnel run --token "$my_cf_token"
+read -p "1.后台运行，2.作为服务运行，请输入选择 (1/2): " MODE_CHOICE
 
-#install a service to automatically run your tunnel whenever your machine starts:
-
-sudo cloudflared service install "$my_cf_token"
-
+if [ "$MODE_CHOICE" = "1" ]; then
+   #run the tunnel manually in your current terminal session only:
+   nohup cloudflared-linux-amd64 tunnel run --token "$my_cf_token" &
+else
+   #install a service to automatically run your tunnel whenever your machine starts:
+   #sudo cloudflared-linux-amd64 service install "$my_cf_token"
+fi
